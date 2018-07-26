2de402ca456c99f9ca18e3e4add3ef7acb4a56e1
Guilherme Bail <guilhemedanbail@gmail.com>
2018-07-26 17:49:40 -0300

    ngFor, ngModel

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,10 +2,80 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: "<span>{{message}}</span>",
+    template: require("./app.component.html"),
+    styles: [
+        require("./app.component.scss")
+    ],
 })
 export class AppComponent {
 
-    public message: string = "Hello World!";
+    public header: string = "Color Tool";
+
+    public message: string = "Color List";
+
+    public colors: string[] = [
+        "red",
+        "white",
+        "green",
+        "blue",
+        "orange",
+    ];
+
+    public newcolor: string = "";
+
+    public addColor()
+    {
+        this.colors.push(this.newcolor);
+        this.newcolor = "";
+    }
+
+    public someNumber: number = 3.14;
+
+    public someDate: Date = new Date();
+
+    public someCurrency: number = 6500;
+
+    public currentPage: number = 0;
+    public pageLength: number = 3;
+
+    public get startPerson(): number {
+        return this.currentPage * this.pageLength;
+    }
+
+    public get endPerson(): number {
+        return (this.currentPage + 1) * this.pageLength;
+    }
+
+    public people: any[] = [
+        { nome: "Guilherme", sobrenome: "Bail"},
+        { nome: "Will", sobrenome: "Bail"},
+        { nome: "John", sobrenome: "Doe"},
+        { nome: "Jane", sobrenome: "Doe"},
+        { nome: "Agent", sobrenome: "Smith"},
+        { nome: "Duke", sobrenome: "Nuken"},
+        { nome: "Perna", sobrenome: "Longa"},
+        { nome: "Darth", sobrenome: "Vader"},
+        { nome: "Peter", sobrenome: "Parker"},
+        { nome: "Clark", sobrenome: "Kent"},
+        { nome: "Bruce", sobrenome: "Wayne"},
+    ];
+
+    prevPage() {
+        if (this.currentPage > 0) {
+            this.currentPage--;
+        }
+    }
+
+    nextPage() {
+        let pages = this.people.length / this.pageLength;
+
+        if (this.people.length % this.pageLength > 0) {
+            pages++;
+        }
+
+        if (this.currentPage < pages) {
+            this.currentPage++;
+        }
+    }
 
 }
--- src/ts/app/app.module.ts
+++ src/ts/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 
 import "../../scss/styles.scss";
 
 @NgModule({
-    imports: [ BrowserModule ],
+    imports: [ BrowserModule, FormsModule ],
     declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ],
+    bootstrap: [ AppComponent],
 })
 export class AppModule { }
