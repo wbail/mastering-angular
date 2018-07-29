7aefc3b2e0a7a90f1a4d37f7e9b23586c039c0d1
Guilherme Bail <guilhemedanbail@gmail.com>
2018-07-29 14:53:37 -0300

    forms, nested forms

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,11 +1,45 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, FormArray, FormBuilder } from "@angular/forms";
 
 @Component({
     selector: "main",
-    template: "<span>{{message}}</span>",
+    template: require("./app.component.html"),
 })
 export class AppComponent {
 
-    public message: string = "Hello World!";
+    public profileForm: FormGroup;
 
+    constructor(private fb: FormBuilder) {
+        this.profileForm = this.fb.group({
+            nome: [""],
+            sobrenome: [""],
+            addressGroups: this.fb.array([
+                    this.fb.group({
+                    rua: [""],
+                    cidade: [""],
+                    uf: [""],
+                    cep: [""],
+                })
+            ])
+        })
+    }
+
+    public addAdressGroup() {
+        const fa = this.profileForm.controls["addressGroups"] as FormArray;
+
+        fa.push(this.newAddressGroup())
+    }
+
+    public save() {
+        console.log(this.profileForm.value);
+    }
+
+    private newAddressGroup() {
+        return new FormGroup({
+            rua: new FormControl(""),
+            cidade: new FormControl(""),
+            uf: new FormControl(""),
+            cep: new FormControl(""),
+        });
+    }
 }
--- src/ts/app/app.module.ts
+++ src/ts/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 
 import "../../scss/styles.scss";
 
 @NgModule({
-    imports: [ BrowserModule ],
+    imports: [ BrowserModule, ReactiveFormsModule ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ],
 })
